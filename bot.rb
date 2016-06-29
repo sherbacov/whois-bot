cc95c50003f192abd17913cbc8ee128bf3318bd3
dunkkle <duncanshea@gmail.com>
2016-06-29 09:29:57 -0700

    trying dns instead

--- bot.rb
+++ bot.rb
@@ -29,20 +29,18 @@ end
 
 def json_response_test(response_url, response)
 
-		data_output = {text: response}
-		json_headers = {"Content-Type" => "application/json", "Accept" => "application/json"}
-		#data_output = data_output
-		
-		uri = URI.parse(response_url)
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		res = http.post(uri.path, data_output.to_json, json_headers)
+  data_output = {text: response}
+  json_headers = {"Content-Type" => "application/json", "Accept" => "application/json"}
+  uri = URI.parse(response_url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  res = http.post(uri.path, data_output.to_json, json_headers)
 
 end
 
 # Define methods - Whois and DNS/Host..
 
-def whois_query(domain, response_url)
+def whois_query(domain)
   whois = Whois::Client.new
 
   # run a Whois query on the domain we're passing in. Returns an object, but can be called as a string
@@ -54,56 +52,57 @@ def whois_query(domain, response_url)
 
   # Return only some specific info...
 
-  domain_that_was_queried    = result.domain
-  domain_created_on_date     = result.created_on #Time/Nil
-  domain_updated_date        = result.updated_on #Time/Nil
-  domain_expiration_date     = result.expires_on
-  domain_registrar           = result.registrar
-  domain_nameservers         = result.nameservers
+  domain_that_was_queried = result.domain
+  domain_created_on_date = result.created_on #Time/Nil
+  domain_updated_date = result.updated_on #Time/Nil
+  domain_expiration_date = result.expires_on
+  domain_registrar = result.registrar
+  domain_nameservers = result.nameservers
   domain_registrant_contacts = result.registrant_contacts
 
   # # Let's output some of this stuff, to see if it's working.
 
   # # need to put all this in a JSON POST message...
 
-   puts "You asked about" + domain_that_was_queried + "...here's what I know: "
-   puts "Registered at:" + domain_registrar.name
+  puts "You asked about" + domain_that_was_queried + "...here's what I know: "
+  puts "Registered at:" + domain_registrar.name
   # puts "Expires on:" + domain_expiration_date.to_s
   # puts "Contact info:" + domain_registrant_contacts.to_s
   # puts domain_nameservers
 
-  response = "name.com yo"
-
-  json_response_test(response_url, response)
+  @response = "name.com yo"
 
 end
 
 
+def dns_query(domain)
+	packet = Net::DNS::Resolver.start(domain)
+
+	header = packet.header
+	answer = packet.answer
+
+	puts "The packet is #{packet.data.size} bytes"
+	puts "It contains #{header.anCount} answer entries"
+
+	answer.any? {|ans| p ans}
+
+	@dns_response = answer.to_s
+	
+end
+
+def main
+  domain = params.fetch('text').strip
+  user_name = params.fetch('user_name')
+  response_url = params.fetch('response_url')
+
+  'Let me check on that for you! Please hold...checking WHOIS for '+ domain
+  #whois_query(domain)
+  dns_query(domain)
+  json_response_test(response_url, @dns_response)
+end
 
 
 # Now the fun starts. Once someone POSTs to this app, it will return information.
-post '/'  do
-	domain = params.fetch('text').strip
-	user_name = params.fetch('user_name')
-	response_url = params.fetch('response_url')
-
-	# case domain
-
-	# 	when 'yo mama'
-	# 		'MUAHAHAH THIS KINDA WORKS ALREADY'
-	# 	end
-
-	# 	when ''
-	# 		'You need to provide a domain name! For example: google.com'
-	# 	end
-
-	# else
-	'Let me check on that for you! Please hold...checking WHOIS for '+ domain + response_url
-	whois_query(domain, response_url)
-	
-
-		#whois_query(domain)
-
-
+post '/' do
+  main
 end
-
